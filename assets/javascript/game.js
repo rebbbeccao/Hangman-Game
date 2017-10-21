71b611d83c0104295035ffb8b4685cc9ed414d5f
Rebecca A. Olson <rebeccaao@Rebeccas-Air.attlocal.net>
2017-10-20 23:16:00 -0700

    Rewritten Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,31 @@ var words = ['crown', 'thirdeye', 'throat', 'heart', 'solarplexus', 'sacral', 'r
 // Choose word randomly
 let randNum = Math.floor(Math.random() * words.length);
 let chosenWord = words[randNum];
-let wins = [];
-let missed = [];
+let correctLetters = [];
+let wrongGuess = true;
+let correctGuesses = 0;
+let wrongLetters = [];
+let guessAmount = 24;
+let wins = 0;
 let underScore = [];
 
 // Dom manipulation
 let docUnderScore = document.getElementsByClassName("underscores");
 let docMissed = document.getElementsByClassName("letters-guessed");
+
+function resetGame() {
+	var words = ['crown', 'thirdeye', 'throat', 'heart', 'solarplexus', 'sacral', 'root'];
+
+// Choose word randomly
+	
+	correctLetters = [];
+	wrongGuess = true;
+	correctGuesses = 0;
+	wrongLetters = [];
+	guessAmount = 24;
+	
+	underScore = [];
+}
 
 console.log(chosenWord);
 
@@ -27,40 +45,57 @@ console.log(chosenWord);
 		}
 		return underScore;
 	}
-console.log(generateUnderScore());	
 
-// Get user's guess
-document.addEventListener('keypress', (event) => {
-	let keyword = String.fromCharCode(event.keycode);
+// Took user guess (letterkey)
+document.onkeyup = function (event) {
+	var userGuess = event.key;
+	console.log(userGuess);
+	wrongGuess = true;
 
-console.log(keyword);	
+// The user guess matches an index of the chosenWord
+if (guessAmount == 0) {	// if amount of guesses is 0, game over - you lose!
+// alert try again!
+// reload the game
+console.log('game over');
 
-// If users guess is right
-		if(chosenWord.indexOf(keyword) > -1) {
-// adds to right word array
-			wins.push(keyword);
-		
-// replace underscore with right letter
-			underScore[chosenWord.indexOf(keyword)] = keyword;
-			docUnderScore[0].innerHTML = underScore.join(' ');	
-			docWins[0].innerHTML = wins.join('');
-// checks to see if user guesses match chosen word 
-		
-		if(underScore.join(' ') == chosenWord) {
-			alert('You win!');
-			}
-		} 
-		else {
-			missed.push(keyword);
-		}	
-});	
+	}
+
+for (let j = 0; j < chosenWord.length; j ++) {
+				if (userGuess == chosenWord.charAt(j)) {       //if the user guess matches a letter in the chosenWord
+					//store letter
+					correctLetters.push(userGuess); //stores correct user guess
+					console.log('array: ' + correctLetters);
+					correctGuesses ++; // adds guess to the correct guess counter
+					wrongGuess = false;
+					//display in #underscores
+
+				console.log('correct guesses amount:' + correctGuesses);
+				console.log("chosenWord @ J is: " + chosenWord[j]);
+				}
+
+										if (correctGuesses === chosenWord.length) { // takes correctGuesses counter and if the amount = chosenWord.length YOU WIN!
+										//alert you win
+										//change image left-img
+										//reload game
+											wins ++;
+											console.log('win number: ' + wins);
 
 
+										}
+}			
+				if (wrongGuess == true) {                                         //if the users guess doesnt match the chosenWord
+				//store wrong guessed letter
+				//push to missed array
+					wrongLetters.push(userGuess); //stores wrong user guess
+					guessAmount --; // subtracts 1 guess from the user guess amount counter
+					console.log('array: ' + wrongLetters);
+					console.log('guess Amount: ' + guessAmount);
 
 
+				}
+}
 
 
-docUnderScore[0].innerHTML = generateUnderScore().join(' ');	
 			
 
 	

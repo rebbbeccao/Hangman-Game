c3070cb439cb0787a27eb4b8bb2f82e553591208
Rebecca A. Olson <rebeccaao@Rebeccas-MacBook-Air.local>
2017-10-20 20:23:18 -0700

    upload for help

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,7 @@ let underScore = [];
 
 // Dom manipulation
 let docUnderScore = document.getElementsByClassName("underscores");
+let docMissed = document.getElementsByClassName("letters-guessed");
 
 console.log(chosenWord);
 
@@ -32,17 +33,20 @@ console.log(generateUnderScore());
 document.addEventListener('keypress', (event) => {
 	let keyword = String.fromCharCode(event.keycode);
 
+console.log(keyword);	
+
 // If users guess is right
 		if(chosenWord.indexOf(keyword) > -1) {
 // adds to right word array
 			wins.push(keyword);
-			docUnderScore.innerHTML = underScore.join;
-			console.log(docUnderScore.innerHTML);
+		
 // replace underscore with right letter
 			underScore[chosenWord.indexOf(keyword)] = keyword;
 			docUnderScore[0].innerHTML = underScore.join(' ');	
+			docWins[0].innerHTML = wins.join('');
 // checks to see if user guesses match chosen word 
-		if(underScore.join('') == chosenWord) {
+		
+		if(underScore.join(' ') == chosenWord) {
 			alert('You win!');
 			}
 		} 
@@ -53,9 +57,10 @@ document.addEventListener('keypress', (event) => {
 
 
 
-console.log(underScore);
-generateUnderScore().join(' ')
-docUnderScore[0].innerHTML = 'Working';	
+
+
+
+docUnderScore[0].innerHTML = generateUnderScore().join(' ');	
 			
 
 	

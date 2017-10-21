044e2973d47391910b6b29e8b3e4a1e5b7e62aa5
Rebecca A. Olson <rebeccaao@Rebeccas-MacBook-Air.local>
2017-10-20 19:21:56 -0700

    mid-way upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,69 @@
+// Global Variables
+//==============================================================================================
+// Create an array of words
+var words = ['crown', 'thirdeye', 'throat', 'heart', 'solarplexus', 'sacral', 'root'];
+
+// Choose word randomly
+let randNum = Math.floor(Math.random() * words.length);
+let chosenWord = words[randNum];
+let wins = [];
+let missed = [];
+let underScore = [];
+
+// Dom manipulation
+let docUnderScore = document.getElementsByClassName("underscores");
+
+console.log(chosenWord);
+
+// Main
+//==============================================================================================
+
+
+// Project underscores based on the length of the chosen word
+	let generateUnderScore = () => {
+		for(let i = 0; i < chosenWord.length; i ++) {
+			underScore.push('_');
+		}
+		return underScore;
+	}
+console.log(generateUnderScore());	
+
+// Get user's guess
+document.addEventListener('keypress', (event) => {
+	let keyword = String.fromCharCode(event.keycode);
+
+// If users guess is right
+		if(chosenWord.indexOf(keyword) > -1) {
+// adds to right word array
+			wins.push(keyword);
+			docUnderScore.innerHTML = underScore.join;
+			console.log(docUnderScore.innerHTML);
+// replace underscore with right letter
+			underScore[chosenWord.indexOf(keyword)] = keyword;
+			docUnderScore[0].innerHTML = underScore.join(' ');	
+// checks to see if user guesses match chosen word 
+		if(underScore.join('') == chosenWord) {
+			alert('You win!');
+			}
+		} 
+		else {
+			missed.push(keyword);
+		}	
+});	
+
+
+
+console.log(underScore);
+generateUnderScore().join(' ')
+docUnderScore[0].innerHTML = 'Working';	
+			
+
+	
+			
+	
+
+
+
+
+
+
